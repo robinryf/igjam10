30589a1e05041f90706a4ac7c3502a9a0ab3fd96
robinryf <robinryf@gmail.com>
2016-10-28 23:06:31 +0200

    Add PlayerMovementController to move player

---
+++ Unity/Assets/Source/Character/PlayerMovementController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    [HideInInspector]
+    public bool facingRight = true;
+    [HideInInspector]
+    public bool dash = false;
+    public float moveForce = 365f;
+    public float maxSpeed = 5f;
+    public float jumpForce = 1000f;
+    public Transform groundCheck;
+
+
+    private bool moving = false;
+    private Rigidbody2D rigid;
+    public float DashDistance;
+
+
+    // Use this for initialization
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moving = rigid.velocity.Equals(Vector2.zero) == false;
+        
+        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Joystick1Button0)) && moving)
+        {
+            dash = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        var input = new Vector2(h, v) * maxSpeed;
+
+        rigid.AddForce(input);
+
+        if (dash)
+        {
+            var dashVector = rigid.velocity * DashDistance;
+            rigid.AddForce(dashVector);
+            dash = false;
+        }
+    }
+}
