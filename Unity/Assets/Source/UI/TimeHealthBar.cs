47bd2d3f4af82933568613cf27e020a1c0c722a6
Frederik Bauer <frederik.bauer@innogames.com>
2016-10-29 22:48:29 +0200

    Handle the easter egg to completely refill the health bar

--- Unity/Assets/Source/UI/TimeHealthBar.cs
+++ Unity/Assets/Source/UI/TimeHealthBar.cs
@@ -3,22 +3,23 @@ using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
 
-public class TimeHealthBar : MonoBehaviour {
+public class TimeHealthBar : MonoBehaviour
+{
 
-	public float startingTimeHealth = 100;
-	public Slider timeHealthSlider;
-	public Image sliderFillImage;
-	public Color damagedColor = new Color (1f, 1f, 0);
-	public Color healColor = new Color (0, 0, 1f);
-	public Color normalColor = new Color(0, 1f, 0);
-	public Color criticalColor = new Color(1f, 0, 0);
+    public float startingTimeHealth = 100;
+    public Slider timeHealthSlider;
+    public Image sliderFillImage;
+    public Color damagedColor = new Color(1f, 1f, 0);
+    public Color healColor = new Color(0, 0, 1f);
+    public Color normalColor = new Color(0, 1f, 0);
+    public Color criticalColor = new Color(1f, 0, 0);
     public Text TimeDigitsText;
-	public float criticalThreshold = 30;
-	public float flashSpeed;
-	public float dyingRate = 1;
+    public float criticalThreshold = 30;
+    public float flashSpeed;
+    public float dyingRate = 1;
     public float AddTimeOnCorrectCode = 20;
-	bool damaged = false;
-	bool healing = false;
+    bool damaged = false;
+    bool healing = false;
 
     private float timeLeft;
 
@@ -29,36 +30,48 @@ public class TimeHealthBar : MonoBehaviour {
         Instance = this;
     }
 
-	void Start ()
-	{
+    void Start()
+    {
         CodeGenerationRunner.Instance.CorrectEvent += OnCorrectCodeEntred;
-	    timeLeft = startingTimeHealth;
-		timeHealthSlider.minValue = 0f;
-		timeHealthSlider.maxValue = startingTimeHealth;
-		sliderFillImage.color = normalColor;
-		timeHealthSlider.value = startingTimeHealth;
-	}
+        timeLeft = startingTimeHealth;
+        timeHealthSlider.minValue = 0f;
+        timeHealthSlider.maxValue = startingTimeHealth;
+        sliderFillImage.color = normalColor;
+        timeHealthSlider.value = startingTimeHealth;
+    }
 
     private void OnCorrectCodeEntred(string s, CodeGenerator.DifficultyType difficulty)
     {
-        AddTimeHealth(AddTimeOnCorrectCode * ((int) difficulty + 1));
+        if (s.Equals("sudo make me a sandwich"))
+        {
+            RefillTimeHealth();
+            return;
+        }
+
+        AddTimeHealth(AddTimeOnCorrectCode * ((int)difficulty + 1));
     }
 
     private void OnDestroy()
     {
         CodeGenerationRunner.Instance.CorrectEvent -= OnCorrectCodeEntred;
     }
-		
-	void Update () {
-		if (damaged) {
-			sliderFillImage.color = damagedColor;
-		} else if (healing) {
-			sliderFillImage.color = healColor;
-		} else {
-			ShowStableColor ();
-		}
-		DecrementTimeHealth ();
-	}
+
+    void Update()
+    {
+        if (damaged)
+        {
+            sliderFillImage.color = damagedColor;
+        }
+        else if (healing)
+        {
+            sliderFillImage.color = healColor;
+        }
+        else
+        {
+            ShowStableColor();
+        }
+        DecrementTimeHealth();
+    }
 
     private void DrawTime()
     {
@@ -71,50 +84,67 @@ public class TimeHealthBar : MonoBehaviour {
         TimeDigitsText.text = string.Format("{0}:{1}", ts.TotalSeconds, ts.Milliseconds);
     }
 
-	void ShowStableColor() {
-		if (timeLeft < criticalThreshold) {
-			TransitionSliderBackgroundColor (criticalColor);
-		} else {
-			TransitionSliderBackgroundColor (normalColor);
-		}
-	}
+    void ShowStableColor()
+    {
+        if (timeLeft < criticalThreshold)
+        {
+            TransitionSliderBackgroundColor(criticalColor);
+        }
+        else
+        {
+            TransitionSliderBackgroundColor(normalColor);
+        }
+    }
 
-	void TransitionSliderBackgroundColor(Color color) {
-		sliderFillImage.color = Color.Lerp (sliderFillImage.color, color, flashSpeed * Time.deltaTime);
-	}
+    void TransitionSliderBackgroundColor(Color color)
+    {
+        sliderFillImage.color = Color.Lerp(sliderFillImage.color, color, flashSpeed * Time.deltaTime);
+    }
 
-	public void AddTimeHealth(float amount) {
-		healing = true;
-		ModifyTimeHealth (amount);
-		healing = false;
-	}
+    public void RefillTimeHealth()
+    {
+        healing = true;
+        ModifyTimeHealth(this.startingTimeHealth - this.timeLeft);
+        healing = false;
+    }
 
-	public void RemoveTimeHealth(float amount) {
-		damaged = true;
-		ModifyTimeHealth(-amount);
-		CheckIfDead ();
-		damaged = false;
-	}
+    public void AddTimeHealth(float amount)
+    {
+        healing = true;
+        ModifyTimeHealth(amount);
+        healing = false;
+    }
 
-	void ModifyTimeHealth(float amount)
-	{
-	    timeLeft += amount;
+    public void RemoveTimeHealth(float amount)
+    {
+        damaged = true;
+        ModifyTimeHealth(-amount);
+        CheckIfDead();
+        damaged = false;
+    }
+
+    void ModifyTimeHealth(float amount)
+    {
+        timeLeft += amount;
         DrawTime();
-	}
+    }
 
-	void CheckIfDead() {
-		if (timeLeft <= 0) {
-			Death ();
-		}
-	}
+    void CheckIfDead()
+    {
+        if (timeLeft <= 0)
+        {
+            Death();
+        }
+    }
 
-	void Death()
-	{
-	    LevelManager.Instance.GameOver();
-	}
+    void Death()
+    {
+        LevelManager.Instance.GameOver();
+    }
 
-	void DecrementTimeHealth() {
-		ModifyTimeHealth(dyingRate * Time.deltaTime * (-1));
-		CheckIfDead ();
-	}
+    void DecrementTimeHealth()
+    {
+        ModifyTimeHealth(dyingRate * Time.deltaTime * (-1));
+        CheckIfDead();
+    }
 }
