7186a24c0987da0f0effa6f06b4cef65b26eed14
robinryf <robinryf@gmail.com>
2016-10-29 14:35:14 +0200

    Player Entry and Exit provide start and end points in levels

---
+++ Unity/Assets/Source/Interactable/PlayerExit.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerExit : MonoBehaviour
+{
+    public void OnTriggerEnter2D(Collider2D collider)
+    {
+        // Check if we are colliding with player.
+        if (collider.gameObject.tag == "Player")
+        {
+            // Player has reached us.
+            LevelManager.Instance.EndLevel();
+        }
+    }
+}
