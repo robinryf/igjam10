667b6409859d6f87c7a24d7c2e7861e17cb1fad4
robinryf <robinryf@gmail.com>
2016-10-29 20:43:15 +0200

    Add menu buttons to show in main game

---
+++ Unity/Assets/Source/UI/MenuButtons.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour
+{
+    public void Retry()
+    {
+        LevelManager.Instance.Retry();
+    }
+
+    public void Quit()
+    {
+        SceneManager.LoadScene("start");
+    }
+}
