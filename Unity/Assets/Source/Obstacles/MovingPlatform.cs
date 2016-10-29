2f3a481b00d3aeea94a75a614625c450b72b8a5f
robinryf <robinryf@gmail.com>
2016-10-29 23:19:21 +0200

    Changed position to relative

--- Unity/Assets/Source/Obstacles/MovingPlatform.cs
+++ Unity/Assets/Source/Obstacles/MovingPlatform.cs
@@ -3,22 +3,25 @@ using System.Collections;
 
 public class MovingPlatform : MonoBehaviour {
 
-	public Vector2 startPosition;
-	public Vector2 endPosition;
+	public Vector2 delta;
 	public float speed;
+    private Vector2 savedOrigin;
+    private Vector2 savedTarget;
 
 	// Use this for initialization
-	void Start () {
-		gameObject.transform.position = startPosition;
+	void Start ()
+	{
+	    savedOrigin = transform.position;
+	    savedTarget = savedOrigin + delta;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		gameObject.transform.position = Vector2.MoveTowards (transform.position, endPosition, speed * Time.deltaTime);
-		if ((Vector2) gameObject.transform.position == endPosition) {
-			Vector2 oldEndPosition = endPosition;
-			endPosition = startPosition;
-			startPosition = oldEndPosition;
+		gameObject.transform.position = Vector2.MoveTowards (transform.position, savedTarget, speed * Time.deltaTime);
+		if ((Vector2) gameObject.transform.position == savedTarget) {
+			Vector2 oldEndPosition = savedTarget;
+			savedTarget = savedOrigin;
+			savedOrigin = oldEndPosition;
 		}
 	}
 }
