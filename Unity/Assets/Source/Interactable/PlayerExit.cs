e466333c1c0e5f974e89579e9b6d226384982e53
Frederik Bauer <frederik.bauer@innogames.com>
2016-10-30 09:45:20 +0100

    Add a sound when reaching the Exit

--- Unity/Assets/Source/Interactable/PlayerExit.cs
+++ Unity/Assets/Source/Interactable/PlayerExit.cs
@@ -3,11 +3,14 @@ using System.Collections;
 
 public class PlayerExit : MonoBehaviour
 {
+    public AudioSource ExitSound;
+
     public void OnTriggerEnter2D(Collider2D collider)
     {
         // Check if we are colliding with player.
         if (collider.gameObject.tag == "Player")
         {
+            this.ExitSound.Play();
             if (LevelManager.Instance == null)
             {
                 Debug.LogWarning("You are playing the level directly or the level manager was destroyed. Please use the start scene.");
