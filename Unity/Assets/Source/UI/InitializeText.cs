e158571bedd480862aecb995bd8beaede0ed41c4
robinryf <robinryf@gmail.com>
2016-10-30 10:23:21 +0100

    Add initialize text to scenes

---
+++ Unity/Assets/Source/UI/InitializeText.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class InitializeText : MonoBehaviour
+{
+    public Text Text;
+    public static InitializeText Instance { get; private set; }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void SetText(string t)
+    {
+        Text.text = t;
+    }
+}
