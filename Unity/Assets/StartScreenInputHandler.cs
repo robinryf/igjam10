dc39ed73dcf0c83c2994ab1f4f2309301879b408
carolineclark <carolinejaneclark@gmail.com>
2016-10-30 12:27:54 +0100

    Added start screen with ENTER functionality

---
+++ Unity/Assets/StartScreenInputHandler.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+
+public class StartScreenInputHandler : MonoBehaviour {
+
+	public EventTrigger.TriggerEvent customCallback;
+	private InputField input;
+
+	void Start () 
+	{
+		input = gameObject.GetComponent<InputField>();
+		input.onEndEdit.AddListener(SubmitName);
+	}
+
+	void OnLevelWasLoaded(int level)
+	{
+		EventSystem.current.SetSelectedGameObject(input.gameObject, null);
+		input.OnPointerClick (new PointerEventData(EventSystem.current));
+	}
+
+	private void SubmitName(string arg0)
+	{
+		if (arg0.ToLower() == "enter") {
+			GoToNextScreen ();
+		}
+		input.text = "";
+	}
+
+	private void GoToNextScreen() {
+		BaseEventData eventData= new BaseEventData(EventSystem.current);
+		eventData.selectedObject=this.gameObject;
+		customCallback.Invoke (eventData);
+	}
+}
