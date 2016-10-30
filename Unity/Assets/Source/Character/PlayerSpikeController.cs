56809e053e98950ecfc8b48ee2165825858bbbc8
Frederik Bauer <frederik.bauer@innogames.com>
2016-10-30 10:11:48 +0100

    Add code to play bouncing and hitting sound then the play will be bounced or hit

--- Unity/Assets/Source/Character/PlayerSpikeController.cs
+++ Unity/Assets/Source/Character/PlayerSpikeController.cs
@@ -1,7 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerSpikeController : MonoBehaviour {
+public class PlayerSpikeController : MonoBehaviour
+{
+    public AudioSource bouncingSound;
+    public AudioSource hittingSound;
 
 	private Rigidbody2D rigid;
 	public float force = 200;
@@ -11,17 +14,31 @@ public class PlayerSpikeController : MonoBehaviour {
 		rigid = gameObject.GetComponent<Rigidbody2D> ();
 	}
 
-	void OnCollisionEnter2D(Collision2D other) {
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag != "Spikes" && other.transform.parent.gameObject.tag != "Spikes") return;
+
+        hittingSound.loop = true;
+        hittingSound.Play();
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag != "Spikes" && other.transform.parent.gameObject.tag != "Spikes") return;
+
+        hittingSound.loop = false;
+    }
+
+    void OnCollisionEnter2D(Collision2D other) {
 		if (other.gameObject.tag == "Spikes" || other.transform.parent.gameObject.tag == "Spikes") {
 			Vector2 newPlayerVelocity = other.relativeVelocity * -1 * force;
 			if (newPlayerVelocity.magnitude > maxSpeed) {
 				newPlayerVelocity = Vector2.ClampMagnitude (newPlayerVelocity, maxSpeed);
 			}
 			rigid.AddForce(newPlayerVelocity);
+            bouncingSound.Play();
 		}
 	}
-
-
 
 // Below is code for removing health with trigger instead of bouncing back using collider.
 
