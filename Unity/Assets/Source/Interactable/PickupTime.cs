58663d411d603a681e163dcbe4cabc4374e5680c
Frederik Bauer <frederik.bauer@innogames.com>
2016-10-30 11:32:20 +0100

    Sound will be played when collecting coin

--- Unity/Assets/Source/Interactable/PickupTime.cs
+++ Unity/Assets/Source/Interactable/PickupTime.cs
@@ -3,8 +3,9 @@ using System.Collections;
 
 public class PickupTime : MonoBehaviour
 {
-
+    public AudioSource pickSound;
     public int bonus = 20;
+    private bool picked = false;
     GameObject player;
 
     void Awake()
@@ -12,11 +13,24 @@ public class PickupTime : MonoBehaviour
         player = GameObject.FindGameObjectWithTag("Player");
     }
 
+    void Update()
+    {
+        if (!picked) return;
+
+        if (!pickSound.isPlaying)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject != player) return;
 
+        picked = true;
+        pickSound.Play();
         TimeHealthBar.Instance.AddTimeHealth(bonus);
-        gameObject.SetActive(false);
+        gameObject.GetComponent<SpriteRenderer>().enabled = false;
+        gameObject.GetComponent<Collider2D>().enabled = false;
     }
 }
