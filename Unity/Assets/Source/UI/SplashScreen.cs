13b9d22dc1f234921e8f84ee0154bafc3bfc29b9
robinryf <robinryf@gmail.com>
2016-10-30 13:00:23 +0100

    Set correct splashscreen fade in times

--- Unity/Assets/Source/UI/SplashScreen.cs
+++ Unity/Assets/Source/UI/SplashScreen.cs
@@ -7,24 +7,36 @@ public class SplashScreen : MonoBehaviour
     public float FadeTime = 2;
     public Image FadeImage;
 
+    public float InitialTime = 2;
+    
     public CanvasGroup MainMenuGroup;
 
+    private float currentInitialTime;
     private float currentFadeTime;
 
     private void Start()
     {
+        currentInitialTime = InitialTime;
         currentFadeTime = FadeTime;
     }
 
     private void Update()
     {
+
+        if (currentInitialTime >= 0)
+        {
+            currentInitialTime -= Time.deltaTime;
+            return;
+        }
+
+
         currentFadeTime -= Time.deltaTime;
 
         var col = FadeImage.color;
         col.a = Mathf.Lerp(1, 0, 1 - (currentFadeTime / FadeTime));
         FadeImage.color = col;
-        var groupAlpha = Mathf.Lerp(0, 1, 1 - (currentFadeTime / FadeTime));
-        MainMenuGroup.alpha = groupAlpha;
+        //var groupAlpha = Mathf.Lerp(0, 1, 1 - (currentFadeTime / FadeTime));
+        //MainMenuGroup.alpha = groupAlpha;
 
         if (currentFadeTime <= 0)
         {
